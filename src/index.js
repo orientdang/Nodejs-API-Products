15b374f98f28c32f5f59750849486c055dc825cc
Dang Nguyen Hai <dang.nguyen@orientsoftware.com>
2019-04-24 15:13:08 +0700

    DONE GET + POST with MongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const PORT = '3000';
 const express = require('express');
 const productRoute = require('./route/product');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/product', productRoute);
-app.get('/', (req, res) => {
-    res.send(`Hello!!!`);
+app.use(bodyParser.json());
+
+
+// middelware request
+app.use('/', productRoute);
+
+
+// Handeler for 404
+app.use((req, res, next) => {
+    res.status(404).send('page not found');
+})
+// Handler for 505
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.send('505 server');
 })
 
 app.listen(PORT);
--- src/route/product.js
+++ src/route/product.js
@@ -1,8 +1,40 @@
+const ProductModel = require('../models/product.model');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('You have req a products');
+// Create new product
+router.post('/product', (req, res) => {
+    // console.log(req.body);
+    if (req.body) {
+        let model = new ProductModel(req.body);
+        model.save().then((doc) => {
+            if (!doc) {
+                return res.status(500).send(doc);
+            }
+            res.status(201).send(doc);
+        }).catch(err => {
+            res.status(500).json(err);
+        })
+    } else {
+        return res.status(400).send('Request body is missing');
+    }
 })
 
-module.exports = router; 
+router.get('/listproduct', (req, res) => {
+    if (req.body) {
+        ProductModel.find({}, (err, docs) => {
+            if (err) {
+                res.status(500).send('Can not get list products');
+            }
+            let products = [];
+            docs.map((doc)=>{
+                products.push(doc)
+            });
+            res.status(200).send(products);
+        })
+    } else {
+        res.status(404).send('Missing url params');
+    }
+})
+module.exports = router;
+
